6656ec782b7dddd94dfddaf8782697e0bf2f3f4a
Clinton Rizzo <70792821+clintrizzo@users.noreply.github.com>
2022-05-10 13:41:56 -0700

    added models folder

--- angularApp/src/app/app.module.ts
+++ angularApp/src/app/app.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { UserComponent } from './components/users/user.component';
+import { UserComponent } from './components/user/user.component';
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
   ],
   imports: [
     BrowserModule
